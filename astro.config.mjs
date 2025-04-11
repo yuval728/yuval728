bdcae6fbcd2b74ee3c6892c0bace844f398494c6
Yuval728 <yuvalmehta.728@gmail.com>
2025-04-11 20:58:07 +0530

    fix: Update GitHub Actions workflow for Astro deployment and adjust package scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import github from '@astrojs/github';
+// import github from '@astrojs/github';
 
 export default defineConfig({
   integrations: [tailwind(), mdx()],
